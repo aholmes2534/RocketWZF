a65b7893470de02107c12311591dd8bb739f9598
Ashwin Naik <aholmes2534@users.noreply.github.com>
2020-12-25 14:11:54 +0530

    Added thrusting
    The rocket can now thrust using a script, a really basic one, which will be proably polished later.

---
+++ Boost/Assets/Scripts/Rocket.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+       private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) // thrust (while rotating too)
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotate left!"); 
+        }
+
+        else if (Input.GetKey(KeyCode.D)) //else if prevents simultaneous left-right rotation
+        {
+            print("Roate right!");
+        }
+    }
+}
