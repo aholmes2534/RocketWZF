36cfd0b04991d49d43e4798604f1deee98589767
Ashwin Naik <aholmes2534@users.noreply.github.com>
2021-01-06 14:53:47 +0530

    Movement fixes

--- Boost/Assets/Scripts/Rocket.cs
+++ Boost/Assets/Scripts/Rocket.cs
@@ -1,41 +1,45 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    Rigidbody rigidBody;
+    private Rigidbody rigidBody;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-       private void ProcessInput()
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space)) // thrust (while rotating too)
         {
-            AudioSource audioSource=GetComponent<AudioSource>();
+            AudioSource audioSource = GetComponent<AudioSource>();
             rigidBody.AddRelativeForce(Vector3.up);
-            if(!audioSource.isPlaying)
+            if (!audioSource.isPlaying)
                 audioSource.Play();
         }
+    }
 
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; //manual control of the rotation
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.forward);
         }
-
         else if (Input.GetKey(KeyCode.D)) //else if prevents simultaneous left-right rotation
         {
             transform.Rotate(-Vector3.forward);
         }
+
+        rigidBody.freezeRotation = false; //resume physics controlled rotation
     }
 }
