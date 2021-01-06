a6a4d89721db8004a83ba22777facddc94ed8570
Ashwin Naik <aholmes2534@users.noreply.github.com>
2021-01-06 15:40:40 +0530

    Using serialized field and smoothening movements
    Added thrusts for rotation and vertical movement, used serialized fields for editable values via the inspector.

--- Boost/Assets/Scripts/Rocket.cs
+++ Boost/Assets/Scripts/Rocket.cs
@@ -3,6 +3,9 @@
 public class Rocket : MonoBehaviour
 {
     private Rigidbody rigidBody;
+    private AudioSource audioSource;
+    [SerializeField] private float rcsThrust = 100f;
+    [SerializeField] private float mainThrust = 100f;
 
     // Start is called before the first frame update
     private void Start()
@@ -21,8 +24,8 @@ public class Rocket : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space)) // thrust (while rotating too)
         {
-            AudioSource audioSource = GetComponent<AudioSource>();
-            rigidBody.AddRelativeForce(Vector3.up);
+            audioSource = GetComponent<AudioSource>();
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
             if (!audioSource.isPlaying)
                 audioSource.Play();
         }
@@ -30,14 +33,15 @@ public class Rocket : MonoBehaviour
 
     private void Rotate()
     {
+        float rotationSpeed = rcsThrust * Time.deltaTime;
         rigidBody.freezeRotation = true; //manual control of the rotation
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationSpeed);
         }
         else if (Input.GetKey(KeyCode.D)) //else if prevents simultaneous left-right rotation
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationSpeed);
         }
 
         rigidBody.freezeRotation = false; //resume physics controlled rotation
